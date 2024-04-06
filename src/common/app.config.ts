e2c1dc059a9b756f60ca10bd1d499b9c99db830e
a1cy <a1hvdy@gmail.com>
2024-04-06 11:10:50 +0300

    df

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,23 +8,23 @@
  */
 export const Brand = {
   Title: {
-    Base: 'MHG-ai',
-    Common: (process.env.NODE_ENV === 'development' ? '[MHG] ' : '') + 'ai',
+    Base: 'MHG-AI',
+    Common: (process.env.NODE_ENV === 'development' ? '[MHG] ' : '') + 'AI',
   },
   Meta: {
     Description: 'Launch mhg-ai to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
     SiteName: 'mhg-ai | Precision AI for You',
-    ThemeColor: '#32383E',
-    TwitterSite: '@enricoros',
+    ThemeColor: '#006eb6',
+    TwitterSite: '',
   },
   URIs: {
-    Home: 'https://int-mhg.com',
+    Home: 'http://marketing-home.com/',
     // App: 'https://get.big-agi.com',
     CardImage: '',
     OpenRepo: '',
     OpenProject: '',
     SupportInvite: '',
-    // Twitter: 'https://www.twitter.com/enricoros',
+    // Twitter: '',
     PrivacyPolicy: '',
   },
 } as const;
